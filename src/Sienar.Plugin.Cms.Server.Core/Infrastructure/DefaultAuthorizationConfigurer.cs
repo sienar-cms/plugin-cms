fdf3a41675798d2d29be107fb62d3185356fed9c
Christian LeVesque <christian@levesque.dev>
2024-08-09 16:49:42 -0500

    initial commit

---
+++ src/Sienar.Plugin.Cms.Server.Core/Infrastructure/DefaultAuthorizationConfigurer.cs
@@ -0,0 +1,14 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+
+namespace Sienar.Infrastructure;
+
+/// <exclude />
+public class DefaultAuthorizationConfigurer : IConfigurer<AuthorizationOptions>
+{
+	public void Configure(
+		AuthorizationOptions options,
+		IConfiguration config) {}
+}
---
+++ src/Sienar.Plugin.Cms.Server.Ssr/README.md
@@ -0,0 +1,5 @@
+﻿# Sienar.Plugin.Cms.Server.Ssr
+
+This package contains an ASP.NET MVC implementation of Sienar, packaged as a Sienar plugin. To use this plugin, include the `Sienar.Plugins.SienarCmsSsr` plugin in your web application.
+
+For more information, see the [Sienar docs](https://sienar.io).
