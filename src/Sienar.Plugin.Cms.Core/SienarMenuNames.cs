afc83f8ddfe962bb8d4d6bb79c0165ac5f25b061
Christian LeVesque <christian@levesque.dev>
2024-08-16 16:29:28 -0500

    added SienarMenuNames class with consts of menu names within Sienar CMS

---
+++ src/Sienar.Plugin.Cms.Core/SienarMenuNames.cs
@@ -0,0 +1,22 @@
+﻿namespace Sienar;
+
+/// <summary>
+/// Contains names of Sienar-provided menus
+/// </summary>
+public static class SienarMenuNames
+{
+	/// <summary>
+	/// The name of the main menu
+	/// </summary>
+	public const string MainMenu = $"Sienar{nameof(MainMenu)}";
+
+	/// <summary>
+	/// The name of the dashboard main menu
+	/// </summary>
+	public const string DashboardMainMenu = $"Sienar{nameof(DashboardMainMenu)}";
+
+	/// <summary>
+	/// The name of the dashboard info menu
+	/// </summary>
+	public const string InfoMenu = $"Sienar{nameof(InfoMenu)}";
+}
