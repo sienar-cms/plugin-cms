cb3cd956c18618be8768defad081b523e43a7012
christianlevesque <christian@levesque.dev>
2024-10-12 08:08:50 -0500

    added AccountController with Register endpoint

---
+++ src/Sienar.Plugin.Cms.Server/Identity/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sienar.Identity.Requests;
+using Sienar.Infrastructure;
+using Sienar.Services;
+
+namespace Sienar.Identity;
+
+/// <exclude />
+[ApiController]
+[Route("account")]
+[Authorize]
+public class AccountController : ServiceController
+{
+	public AccountController(IReadableNotificationService notifier)
+		: base(notifier) {}
+
+	[HttpPost]
+	[AllowAnonymous]
+	public Task<IActionResult> Register(
+		[FromForm] RegisterRequest data,
+		[FromServices] IStatusService<RegisterRequest> service)
+		=> ExecuteService(service, data);
+}
