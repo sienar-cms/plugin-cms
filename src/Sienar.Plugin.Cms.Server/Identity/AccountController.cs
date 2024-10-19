a28a10072aa5c085d42c5ef70d717cd3072228ab
christianlevesque <christian@levesque.dev>
2024-10-19 15:00:58 -0500

    added AccountController with registration endpoint

---
+++ src/Sienar.Plugin.Cms.Server/Identity/AccountController.cs
@@ -0,0 +1,27 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sienar.Identity.Requests;
+using Sienar.Infrastructure;
+using Sienar.Services;
+
+namespace Sienar.Identity;
+
+/// <exclude />
+[ApiController]
+[Route("/api/account")]
+[Authorize]
+public class AccountController : ServiceController
+{
+	public AccountController(IOperationResultMapper mapper)
+		: base(mapper) {}
+
+	[HttpPost]
+	[AllowAnonymous]
+	public Task<IActionResult> Register(
+		[FromForm] RegisterRequest data,
+		[FromServices] IStatusService<RegisterRequest> service)
+		=> Execute(() => service.Execute(data));
+}
