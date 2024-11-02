348ca7e357dbf6b2cf76d6f5dff9e1c38efe12b7
Christian LeVesque <christian@levesque.dev>
2024-11-02 14:10:41 -0500

    added users controller with CRUD endpoints

---
+++ src/Sienar.Plugin.Cms.Server/Identity/UsersController.cs
@@ -0,0 +1,52 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Sienar.Data;
+using Sienar.Infrastructure;
+using Sienar.Services;
+
+namespace Sienar.Identity;
+
+/// <exclude />
+[ApiController]
+[Route("/api/users")]
+[Authorize(Roles = Roles.Admin)]
+public class UsersController : ServiceController
+{
+	public UsersController(IOperationResultMapper mapper)
+		: base(mapper) {}
+
+	[HttpGet]
+	public Task<IActionResult> Read(
+		[FromQuery] Filter? filter,
+		[FromServices] IEntityReader<SienarUser> service)
+		=> Execute(() => service.Read(filter));
+
+	[HttpGet("{id:guid}")]
+	public Task<IActionResult> Read(
+		Guid id,
+		[FromQuery] Filter? filter,
+		[FromServices] IEntityReader<SienarUser> service)
+		=> Execute(() => service.Read(id, filter));
+
+	[HttpPost]
+	public Task<IActionResult> Create(
+		[FromForm] SienarUser entity,
+		[FromServices] IEntityWriter<SienarUser> service)
+		=> Execute(() => service.Create(entity));
+
+	[HttpPut]
+	public Task<IActionResult> Update(
+		[FromForm] SienarUser entity,
+		[FromServices] IEntityWriter<SienarUser> service)
+		=> Execute(() => service.Update(entity));
+
+	[HttpDelete("{id:guid}")]
+	public Task<IActionResult> Delete(
+		Guid id,
+		[FromServices] IEntityDeleter<SienarUser> service)
+		=> Execute(() => service.Delete(id));
+}
