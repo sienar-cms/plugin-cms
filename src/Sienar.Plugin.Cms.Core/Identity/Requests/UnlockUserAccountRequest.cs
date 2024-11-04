da4d582cba83484842f237d1ce859512336d4c19
Christian LeVesque <christian@levesque.dev>
2024-11-04 11:23:04 -0600

    removed constructor from UnlockUserAccountRequest

--- src/Sienar.Plugin.Cms.Core/Identity/Requests/UnlockUserAccountRequest.cs
+++ src/Sienar.Plugin.Cms.Core/Identity/Requests/UnlockUserAccountRequest.cs
@@ -5,6 +5,4 @@ namespace Sienar.Identity.Requests;
 public class UnlockUserAccountRequest
 {
 	public Guid UserId { get; set; }
-
-	public UnlockUserAccountRequest(Guid userId) => UserId = userId;
 }
