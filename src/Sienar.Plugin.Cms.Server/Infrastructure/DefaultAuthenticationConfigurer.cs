8052a7aff075b452c90e0ff56bfd125a7bac781e
Christian LeVesque <christian@levesque.dev>
2024-11-24 16:01:44 -0600

    updated existing configurers to have correct IConfigurer signature

--- src/Sienar.Plugin.Cms.Server/Infrastructure/DefaultAuthenticationConfigurer.cs
+++ src/Sienar.Plugin.Cms.Server/Infrastructure/DefaultAuthenticationConfigurer.cs
@@ -2,16 +2,13 @@
 
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.Extensions.Configuration;
 
 namespace Sienar.Infrastructure;
 
 /// <exclude />
 public class DefaultAuthenticationConfigurer : IConfigurer<AuthenticationOptions>
 {
-	public void Configure(
-		AuthenticationOptions options,
-		IConfiguration config)
+	public void Configure(AuthenticationOptions options)
 	{
 		options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 		options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
